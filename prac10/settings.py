23c3956fd8e8924d37607d0957456fe11a3625d0
Shrey Viradiya <40735908+Shrey-Viradiya@users.noreply.github.com>
2020-04-29 17:44:37 +0530

    Update settings.py

--- prac10/settings.py
+++ prac10/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm=agk7!%j(!m6+-h*=&8ck@qjf#zo$659@4^&b#&)31$__r27^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['csvreaderapp.herokuapp.com']
 
